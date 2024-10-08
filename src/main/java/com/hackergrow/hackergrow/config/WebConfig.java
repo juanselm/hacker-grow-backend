1829debb845cb0244732254b4228b6085f87a09c
Juan Loaiza <juanmazo1234@gmail.com>
2024-10-07 22:56:50 -0500

    Se agrega politica de cors

---
+++ src/main/java/com/hackergrow/hackergrow/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.hackergrow.hackergrow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permitir todos los orígenes, puedes especificar orígenes específicos si es necesario
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
