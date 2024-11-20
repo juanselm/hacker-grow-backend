0bd219342ac7e212f963f7d962a7b2484494b516
Juan Loaiza <juanmazo1234@gmail.com>
2024-11-19 19:47:14 -0500

    se agrega controller para listar retos por id

--- src/main/java/com/hackergrow/hackergrow/controllers/RetoController.java
+++ src/main/java/com/hackergrow/hackergrow/controllers/RetoController.java
@@ -4,11 +4,10 @@ import com.hackergrow.hackergrow.entities.Reto;
 import com.hackergrow.hackergrow.services.RetoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/retos")
@@ -21,5 +20,11 @@ public class RetoController {
     public ResponseEntity<List<Reto>> listarTodosLosRetos() {
         List<Reto> retos = retoService.obtenerTodosLosRetos();
         return ResponseEntity.ok(retos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reto> obtenerRetoPorId(@PathVariable Long id) {
+        Optional<Reto> reto = retoService.obtenerRetoPorId(id);
+        return reto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/hackergrow/hackergrow/services/RetoService.java
+++ src/main/java/com/hackergrow/hackergrow/services/RetoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RetoService {
@@ -15,5 +16,9 @@ public class RetoService {
 
     public List<Reto> obtenerTodosLosRetos() {
         return retoRepository.findAll();
+    }
+
+    public Optional<Reto> obtenerRetoPorId(Long id) {
+        return retoRepository.findById(id);
     }
 }
